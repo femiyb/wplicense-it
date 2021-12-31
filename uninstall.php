af9d33ac15323a82bb7ff21a198eb325fb527c5e
Femi <94sam.fem@gmail.com>
2021-12-31 21:12:48 +0200

    Uninstall function

--- uninstall.php
+++ uninstall.php
@@ -4,4 +4,17 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
- 
+
+// Delete the table
+global $wpdb;
+
+$table_name = $wpdb->prefix . 'wplit_product_licenses';
+$order_table_name = $wpdb->prefix . 'wplit_orders';
+
+$sql = "DROP TABLE IF EXISTS $table_name";
+$ordertable = "DROP TABLE IF EXISTS $order_table_name";
+
+$wpdb->query($sql);
+$wpdb->query($ordertable);
+
+delete_option("wplit_db_version");
